d5539435054c239870ca5797c24237b2f1935412
abarkley123 <barkley36@gmail.com>
2020-06-08 12:08:46 +0100

    Fix test to assert properly.

--- test/stub_audio_manager.js
+++ test/stub_audio_manager.js
@@ -8,12 +8,6 @@ export class TestAudioManager extends AudioManager {
 
     constructor() {
         super();
-        this.audioFiles = {};
-        this.filePaths = []; 
-        // TODO - consider using service workers to fulfill this.
-        this.setupAudio()
-            .then(() => log("Successfully loaded audio files.", "info"))
-            .catch(e => log("Failed to setup audio due to cause: " + e, "error"));
     }
 
     /* Background music supplied by Eva – 失望した: https://youtu.be/jVTsD4UPT-k, 
@@ -23,24 +17,19 @@ export class TestAudioManager extends AudioManager {
 
     // This function should load all audio files. 
     async setupAudio() {
-        fetch("http://" + config.address + ":" + config.port + '/audio',{
+        fetch("http://" + config.address + ":" + config.port + '/audio', {
             method: "GET", 
             credentials:"omit"
         }).then(response => {
             if (response.status === 200 && response.ok) {
                 response.json().then(data => {
                     data["message"].forEach(file => {
-
-                        this.filePaths.push(file);
-                        let audio = new Audio(file.substring(file.indexOf("public")));
-                        audio.volume = 0.4;
+                        let audio = file.substring(file.indexOf("public"));
                         this.audioFiles[file.substring(file.lastIndexOf("/") + 1, file.lastIndexOf("."))] = audio;
                     });
-
-                    this.setupEventListeners();
                 }).catch(err => {
                     // node doesn't have Audio objects
-                    if (err.message !== "Audio is not defined") log(err.message, "error");
+                    log(err.message, "error");
                 });
             } else {
                 throw Error(response.statusText);
--- test/test_audio_manager.js
+++ test/test_audio_manager.js
@@ -2,20 +2,26 @@ var assert = require('assert');
 import {TestAudioManager} from "./stub_audio_manager.js";
 import app from "../server.mjs";
 
-let server;
+let server, manager;
 
 before(done => {
+  manager = new TestAudioManager();
   server = app.listen(3002, done);
 });
 
+let mochaAsync = (fn) => {
+  return (done) => {
+    fn.call().then(done, (err)=>{done(err)});
+  };
+};
+
 describe('AudioManager', function() {
   describe('constructor()', function() {
-    it('should successfully initialise the audio manager.', function() {
-        let manager = new TestAudioManager();
-
-        // while (true) console.log(manager.filePaths);
-        assert.ok(manager.filePaths.length > 0);
-    });
+    it('should successfully initialise the audio manager.', mochaAsync(async() => {
+        await manager.setupAudio();
+        // check background music was set
+        assert.strictEqual("public/audio/backgroundMain.mp3", manager.audioFiles["backgroundMain"]);
+    }));
   });
 });
 
